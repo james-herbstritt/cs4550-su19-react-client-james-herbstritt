8142f2a84fbdde27bccf0944b7ada1c8d68079c4
James Herbstritt <james.herbstritt@gmail.com>
2019-05-26 02:22:41 -0400

    Add basic course navbar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import CourseList from '../src/containers/CourseList';
+import CourseNavbar from './containers/CourseNavbar'
 
 ReactDOM.render(
     <div>
-        <h1>Hello, world!</h1>
+        <CourseNavbar/>
         <CourseList/>
     </div>,
     document.getElementById('root')
