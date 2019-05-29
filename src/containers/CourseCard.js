5e510b1cac97e3ce17179e151ef231aeb1154841
James Herbstritt <james.herbstritt@gmail.com>
2019-05-29 17:25:34 -0400

    Implement sizing grid changing

--- src/containers/CourseCard.js
+++ src/containers/CourseCard.js
@@ -2,49 +2,26 @@ import React from 'react';
 import  {Link} from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
-/*
-export default class CourseCard extends React.Component {
-    render () {
-        return (
-            <div className="card mr-3" styles={{width: '18rem'}}>
-                <img className="card-img-top"
-                     src="https://picsum.photos/300/200"/>
 
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.course.title}</h5>
-                    <p className="card-text">Last Modified: 14 May 2019</p>
-                    <Link className="btn btn-success btn-block"
-                          to={`/course/edit/${this.props.course.id}`}>
-                        Edit
-                    </Link>
-                        <button className="btn bg-danger wbdv-delete"
-                                type="button"
-                                onClick={() => deleteRow(course.id)}>
-                            <i className="fa fa-times-circle"></i>
-                        </button>
-                </div>
-            </div>
-        )
-    }
-}
-*/
 const CourseCard = ({course, deleteCard}) =>
-    <div className="card mr-3" styles={{width: '18rem'}}>
-        <img className="card-img-top"
-             src="https://picsum.photos/300/200"/>
+    <div className="col-xl-2 col-md-4 col-6">
+        <div className="card mr-3 mb-3 align-items-stretch w-100">
+            <img className="card-img-top"
+                 src="https://picsum.photos/300/200"/>
 
-        <div className="card-body">
-            <h5 className="card-title">{course.title}</h5>
-            <p className="card-text">Last Modified: 14 May 2019</p>
-            <Link className="btn btn-success btn-block"
-                  to={`/course/edit/${course.id}`}>
-                Edit
-            </Link>
-            <button className="btn bg-danger wbdv-delete btn-block"
-                    type="button"
-                    onClick={() => deleteCard(course.id)}>
-                <i className="fa fa-times-circle"></i>
-            </button>
+            <div className="card-body">
+                <h5 className="card-title">{course.title}</h5>
+                <p className="card-text">Last Modified: 14 May 2019</p>
+                <Link className="btn btn-success btn-block"
+                      to={`/course/edit/${course.id}`}>
+                    Edit
+                </Link>
+                <button className="btn bg-danger text-white wbdv-delete btn-block"
+                        type="button"
+                        onClick={() => deleteCard(course.id)}>
+                    Remove
+                </button>
+            </div>
         </div>
     </div>;
 
--- src/containers/CourseGrid.js
+++ src/containers/CourseGrid.js
@@ -4,26 +4,13 @@ import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import CourseCard from './CourseCard';
 import CourseGridHeader from "./CourseGridHeader";
 
-/*
-export default class CourseGrid extends React.Component {
-    render () {
-        return (
-            <div>
-                <CourseGridHeader/>
-                    <div className="card-group container">
-                        { this.props.courses.map((course, key) => <CourseCard course={course} key={key}/>)}
-                    </div>
-            </div>
-        )
-    }
-}
-*/
-
 const CourseGrid = ({courses, deleteCourse}) =>
     <div>
         <CourseGridHeader/>
-        <div className="card-group container">
-            { courses.map((course, key) => <CourseCard deleteCard={deleteCourse} course={course} key={key}/>)}
+        <div className="container">
+            <div className="row">
+                { courses.map((course, key) => <CourseCard deleteCard={deleteCourse} course={course} key={key}/>)}
+            </div>
         </div>
     </div>;
 
