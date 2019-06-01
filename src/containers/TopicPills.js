cec080694064950f3c86946c07c6c307b2122bce
James Herbstritt <james.herbstritt@gmail.com>
2019-06-01 19:07:28 -0400

    Add delete, edit, and input field to topics, no functionality

--- src/containers/TopicPills.js
+++ src/containers/TopicPills.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
-const TopicPills = ({topics, selectTopic, selectedTopic}) =>
-    <ul className="nav nav-pills ">
-        { topics.map((topic, key) =>
-            <li className="nav-item"
-                key={key}
-                onClick={() => selectTopic(topic)}>
-                <a href="#"
-                    className={topic === selectedTopic ?
-                    "nav-link active" :
-                    "nav-link"}>
-                    {topic.title}</a></li>)}
-    </ul>;
 
-export default TopicPills;
+export default class TopicPills extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            topicName: ""
+        }
+    }
+    render() {
+        return (
+            <ul className="nav nav-pills mt-1">
+                { this.props.topics.map((topic, key) =>
+                    <li className="nav-item"
+                        key={key}
+                        onClick={() => this.props.selectTopic(topic)}>
+                        <a href="#"
+                           className={topic === this.props.selectedTopic ?
+                               "nav-link active" :
+                               "nav-link"}>
+                            {topic.title}
+                            <span className="float-right mb-1">
+                                <button className="btn-sm bg-success ml-3 mr-1"
+                                        type="button">
+                                    <i className="fa fa-pencil"></i>
+                                </button>
+                                <button className="btn-sm bg-danger"
+                                        type="button">
+                                    <i className="fa fa-times-circle"></i>
+                                </button>
+                            </span>
+                        </a>
+                    </li>)}
+                <li>
+                    <input className="nav-item form-control bg-light"
+                           placeholder="topic"
+                           value={this.state.topicName}/>
+                </li>
+                <li>
+                    <button className="btn btn-primary btn-block ml-1">
+                        <i className="fa fa-plus-circle"></i>
+                    </button>
+                </li>
+            </ul>
+        )
+    }
+}
