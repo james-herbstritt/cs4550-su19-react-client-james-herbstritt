77caea0e2f4282f6f3d2c740271dee17339e76c7
James Herbstritt <james.herbstritt@gmail.com>
2019-06-07 20:54:06 -0400

    Track changes in file structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import WhiteBoard from "./containers/components/WhiteBoard";
+import WhiteBoard from "./components/WhiteBoard";
 
 ReactDOM.render(
         <WhiteBoard/>,
