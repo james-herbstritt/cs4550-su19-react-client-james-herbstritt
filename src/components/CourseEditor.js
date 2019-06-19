2bc394ba6bd3d6a9b6469369561b5a6037391d5c
James Herbstritt <james.herbstritt@gmail.com>
2019-06-19 16:14:46 -0400

    Implement CourseEditor refactor

--- src/components/CourseEditor.js
+++ src/components/CourseEditor.js
@@ -10,6 +10,9 @@ import WidgetListContainer from "../containers/WidgetListContainer"
 import {createStore} from "redux";
 import {Provider} from 'react-redux';
 import WidgetReducer from '../reducers/WidgetReducer';
+import LessonService from "../services/LessonService";
+import TopicService from "../services/TopicService";
+import ModuleService from "../services/ModuleService";
 
 const store = createStore(WidgetReducer);
 
@@ -23,6 +26,45 @@ export default class CourseEditor extends React.Component {
         }
     }
 
+    createModule = moduleName => {
+        this.props.addModule(moduleName);
+    };
+
+    deleteModule = id => {
+        let lessonService = LessonService.getInstance();
+        let topicService = TopicService.getInstance();
+
+        let lessl = this.state.selectedModule.lessons.length;
+
+        for(let l = lessl; l > 0; l--) {
+            let topsl = this.state.selectedModule.lessons[lessl - 1].topics.length;
+            for(let t = topsl; t > 0; t--) {
+                console.log("removing topic",t, "of lesson ", l);
+                if(t !== 1) {
+                    topicService.deleteTopic(this.state.selectedModule.lessons[l - 1].topics[t - 1].id);
+                }
+                else {
+                    if(l !== 1) {
+                        console.log("removing lesson", l);
+                        topicService.deleteTopic(this.state.selectedModule.lessons[l - 1].topics[t - 1].id)
+                            .then(() => lessonService.deleteLesson(this.state.selectedModule.lessons[l - 1].id));
+                    }
+                    else {
+                        console.log("removing lesson", l);
+                        topicService.deleteTopic(this.state.selectedModule.lessons[l - 1].topics[t - 1].id)
+                            .then(() => lessonService.deleteLesson(this.state.selectedModule.lessons[l - 1].id))
+                            .then( () => {
+                                console.log("deleting module", id);
+                                this.props.deleteModule(id)
+                                    .then(() => this.props.updateCourses());
+                                }
+                            );
+                    }
+                }
+            }
+        }
+    };
+
     selectModule = module =>
         this.setState({selectedModule: module,
                              selectedLesson: module.lessons[0],
@@ -34,58 +76,58 @@ export default class CourseEditor extends React.Component {
         this.setState({selectedTopic: topic});
 
     createLesson = lessonName => {
-        let courseService = CourseService.getInstance();
-        var topics =  [{
-            id: new Date().getTime(),
-            title: "Default Topic"
-        }];
-        var lesson = {
-            id: new Date().getTime(),
-            title: lessonName,
-            topics: topics
-        };
-        this.state.selectedModule.lessons.push(lesson);
-        courseService.updateCourse(this.props.course.id, this.props.course);
-        this.props.updateCourses();
+        let lessonService = LessonService.getInstance();
+        let topicService = TopicService.getInstance();
+
+        let topic =  {title: "Default Topic"};
+        if(lessonName === "") {
+            lessonName = "Default Lesson";
+        }
+        let lesson = {title: lessonName};
+
+         lessonService.createLessonForModule(lesson, this.state.selectedModule.id)
+            .then(response => response[response.length - 1])
+            .then(response => topicService.createTopicForLesson(topic, response.id))
+            .then(response => this.props.updateCourses())
     };
 
     deleteLesson = id => {
-        let courseService = CourseService.getInstance();
+        let lessonService = LessonService.getInstance();
+        let topicService = TopicService.getInstance();
+
+        let topsl = this.state.selectedLesson.topics.length;
+
+       for(let t = topsl; t > 0; t--) {
+           if(t !== 1) {
+               topicService.deleteTopic(this.state.selectedLesson.topics[t - 1].id);
+           }
+           else {
+               topicService.deleteTopic(this.state.selectedLesson.topics[t - 1].id).then( () =>
+                   lessonService.deleteLesson(id).then(() => this.props.updateCourses())
+               )
+           }
+        }
 
         this.state.selectedModule.lessons = this.state.selectedModule.lessons.filter(lesson => lesson.id !== id);
 
-        let newCourse = {
-            id: this.props.course.id,
-            title: this.props.course.title,
-            modules: this.props.course.modules
-        };
-        courseService.updateCourse(this.props.course.id, newCourse);
+
     };
 
     createTopic = topicName => {
-        console.log("creating topic", topicName);
-        let courseService = CourseService.getInstance();
-        var topic = {
-            id: new Date().getTime(),
-            title: topicName
-        };
+        let topicService = TopicService.getInstance();
 
-        this.state.selectedLesson.topics.push(topic);
-        courseService.updateCourse(this.props.course.id, this.props.course);
-        this.props.updateCourses();
+        let topic = {title: topicName};
+
+        topicService.createTopicForLesson(topic, this.state.selectedLesson.id)
+            .then(response => this.props.updateCourses());
     };
 
     deleteTopic = id => {
-        let courseService = CourseService.getInstance();
+        let topicService = TopicService.getInstance();
 
         this.state.selectedLesson.topics = this.state.selectedLesson.topics.filter(topic => topic.id !== id);
 
-        let newCourse = {
-            id: this.props.course.id,
-            title: this.props.course.title,
-            modules: this.props.course.modules
-        };
-        courseService.updateCourse(this.props.course.id, newCourse);
+        topicService.deleteTopic(id).then(() => this.props.updateCourses());
     };
 
     findCourseById = id =>
@@ -94,36 +136,43 @@ export default class CourseEditor extends React.Component {
         });
 
     editModule = (moduleName, moduleId) => {
-        let courseService = CourseService.getInstance();
+        let moduleService = ModuleService.getInstance();
 
         this.props.course.modules.find(function (element){
             return element.id === moduleId;
         }).title = moduleName;
 
-        courseService.updateCourse(this.props.course.id, this.props.course);
-        this.props.updateCourses();
+        let module = {title : moduleName};
+
+        moduleService.updateModuleForCourse(module, this.props.course.id, moduleId)
+            .then(() => this.props.updateCourses())
     };
 
     editLesson = (lessonName, lessonId) => {
-        let courseService = CourseService.getInstance();
+        let lessonService = LessonService.getInstance();
 
         this.state.selectedModule.lessons.find(function (element) {
             return element.id === lessonId;
         }).title = lessonName;
 
-        courseService.updateCourse(this.props.course.id, this.props.course);
-        this.props.updateCourses();
+        let lesson = {title: lessonName};
+
+        lessonService.updateLessonForModule(lesson, this.state.selectedModule.id, lessonId)
+            .then(() => this.props.updateCourses());
     };
 
-    editTopic = (topicName, topicId) => {
-        let courseService = CourseService.getInstance();
+    editTopicForLesson = (topicName, topicId) => {
+        let topicService = TopicService.getInstance();
 
         this.state.selectedLesson.topics.find(function (element) {
             return element.id === topicId;
         }).title = topicName;
 
-        courseService.updateCourse(this.props.course.id, this.props.course);
-        this.props.updateCourses();
+        console.log("updating topic", topicId, "with new name", topicName);
+
+        let topic = {title: topicName};
+        topicService.updateTopicForLesson(topic, this.state.selectedLesson.id, topicId)
+            .then(() => this.props.updateCourses());
     };
 
     render () {
@@ -133,12 +182,15 @@ export default class CourseEditor extends React.Component {
                 <div className="container-fluid">
                     <div className="row mt-3">
                         <div className="col-3">
-                            <ModuleList modules={this.props.course.modules}
+                            {console.log("course",this.props.course)}
+                            {console.log("modules",this.props.course.modules)}
+                             <ModuleList modules={this.props.course.modules}
                                         selectModule={this.selectModule}
                                         selectedModule={this.state.selectedModule}
-                                        deleteModule={this.props.deleteModule}
-                                        createModule={this.props.addModule}
-                                        editModule={this.editModule}/></div>
+                                        deleteModule={this.deleteModule}
+                                        createModule={this.createModule}
+                                        editModule={this.editModule}/>
+                                        </div>
                         <div className="col-9">
                             <LessonTabs lessons={this.state.selectedModule.lessons}
                                         selectLesson={this.selectLesson}
@@ -151,7 +203,7 @@ export default class CourseEditor extends React.Component {
                                         selectedTopic={this.state.selectedTopic}
                                         deleteTopic={this.deleteTopic}
                                         createTopic={this.createTopic}
-                                        editTopic={this.editTopic}/>
+                                        editTopic={this.editTopicForLesson}/>
                              <br/>
                              <Provider store={store}>
                                  <WidgetListContainer/>
